98178f0413321877ba3bd4d3dd130d1ad373c68c
Maurício Bruno <mb.bruno.s@gmail.com>
2024-03-09 16:52:46 -0300

    feat: add database config setup

---
+++ config/db.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"github.com/mbrunos/go-hire/schemas"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB() (*gorm.DB, error) {
+	pass := os.Getenv("MYSQL_ROOT_PASSWORD")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	logger := GetLogger()
+
+	dsn := "root:" + pass + "@tcp(127.0.0.1:3306)/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logger.ErrorF("Error connecting to database: %s", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Job{})
+	if err != nil {
+		logger.ErrorF("Error migrating schema: %s", err)
+		return nil, err
+	}
+
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.22.0
 
 require (
 	github.com/fatih/color v1.16.0
+	gorm.io/driver/mysql v1.5.4
 	gorm.io/gorm v1.25.7
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
