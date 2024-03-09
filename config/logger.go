f592842849d38115013b29eaa6bbdce9693ba21f
Maurício Bruno <mb.bruno.s@gmail.com>
2024-03-09 16:25:20 -0300

    feat: add custom logger

---
+++ config/logger.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+type Logger struct {
+	debug   *log.Logger
+	info    *log.Logger
+	warning *log.Logger
+	err     *log.Logger
+}
+
+func NewLogger() *Logger {
+	writer := io.Writer(os.Stdout)
+
+	return &Logger{
+		debug:   log.New(writer, colorString("[DEBUG] ", color.FgCyan), log.Ldate|log.Ltime|log.Lshortfile),
+		info:    log.New(writer, colorString("[INFO] ", color.FgGreen), log.Ldate|log.Ltime|log.Lshortfile),
+		warning: log.New(writer, colorString("[WARNING] ", color.FgYellow), log.Ldate|log.Ltime|log.Lshortfile),
+		err:     log.New(writer, colorString("[ERROR] ", color.FgRed), log.Ldate|log.Ltime|log.Lshortfile),
+	}
+}
+
+func colorString(s string, c color.Attribute) string {
+	col := color.New(c).SprintFunc()
+	return col(s)
+}
+
+func (l *Logger) Debug(v ...interface{}) {
+	l.debug.Println(v...)
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	l.info.Println(v...)
+}
+
+func (l *Logger) Warning(v ...interface{}) {
+	l.warning.Println(v...)
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	l.err.Println(v...)
+}
+
+func (l *Logger) DebugF(format string, v ...interface{}) {
+	l.debug.Printf(format, v...)
+}
+
+func (l *Logger) InfoF(format string, v ...interface{}) {
+	l.info.Printf(format, v...)
+}
+
+func (l *Logger) WarningF(format string, v ...interface{}) {
+	l.warning.Printf(format, v...)
+}
+
+func (l *Logger) ErrorF(format string, v ...interface{}) {
+	l.err.Printf(format, v...)
+}
