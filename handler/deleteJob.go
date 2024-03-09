836a4f27a368a8d349a0ba42ba782462214304b6
Maurício Bruno <mb.bruno.s@gmail.com>
2024-03-09 18:15:23 -0300

    feat: implement delete job handler

--- handler/deleteJob.go
+++ handler/deleteJob.go
@@ -1,10 +1,26 @@
 package handler
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/mbrunos/go-hire/schemas"
 )
 
 func DeleteJob(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("Delete job by id")
+	id := r.PathValue("id")
+
+	job := schemas.Job{}
+
+	if err := db.First(&job, id).Error; err != nil {
+		sendError(w, http.StatusNotFound, fmt.Errorf("job with id %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&job).Error; err != nil {
+		sendError(w, http.StatusInternalServerError, fmt.Errorf("error deleting job with id %s", id))
+		return
+	}
+
+	sendSuccess(w, http.StatusOK, job)
 }
