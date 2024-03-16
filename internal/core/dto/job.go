e8887f3b101518abd605fe2f706950a4fa503826
Maurício Bruno <mb.bruno.s@gmail.com>
2024-03-16 12:57:33 -0300

    feat: add dtos

---
+++ internal/core/dto/job.go
@@ -0,0 +1,36 @@
+package dto
+
+type CreateJobInputDTO struct {
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Company     string  `json:"company"`
+	Location    *string `json:"location"`
+	Remote      bool    `json:"remote"`
+	Salary      int64   `json:"salary"`
+}
+
+type UpdateJobInputDTO struct {
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Company     string  `json:"company"`
+	Location    *string `json:"location"`
+	Remote      *bool   `json:"remote"`
+	Salary      int64   `json:"salary"`
+}
+
+type JobOutputDTO struct {
+	ID          string  `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Company     string  `json:"company"`
+	Location    *string `json:"location"`
+	Remote      bool    `json:"remote"`
+	Salary      int64   `json:"salary"`
+	CreatedAt   string  `json:"created_at"`
+	UpdatedAt   string  `json:"updated_at"`
+	DeletedAt   *string `json:"deleted_at,omitempty"`
+}
+
+type JobListOutputDTO struct {
+	Jobs []JobOutputDTO `json:"jobs"`
+}
---
+++ internal/core/dto/user.go
@@ -0,0 +1,13 @@
+package dto
+
+type CreateUserInputDTO struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserOutputDTO struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
