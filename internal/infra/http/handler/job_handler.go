5c8076016037996f51c228a558ac3852e8f9fa3c
Maurício Bruno <mb.bruno.s@gmail.com>
2024-03-16 14:41:28 -0300

    feat: finish handlers

--- internal/infra/http/handler/job_handler.go
+++ internal/infra/http/handler/job_handler.go
@@ -2,22 +2,20 @@ package handler
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/mbrunos/go-hire/internal/core/dto"
-	"github.com/mbrunos/go-hire/internal/core/entity/interfaces"
 	"github.com/mbrunos/go-hire/internal/core/usecases"
 )
 
 type JobHandler struct {
-	jobRepository interfaces.JobRepository
+	jobUseCase *usecases.JobUseCase
 }
 
-func NewJobHandler(jobRepository interfaces.JobRepository) *JobHandler {
+func NewJobHandler(usecase *usecases.JobUseCase) *JobHandler {
 	return &JobHandler{
-		jobRepository: jobRepository,
+		jobUseCase: usecase,
 	}
 }
 
@@ -28,11 +26,10 @@ func (h *JobHandler) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	jobUseCase := usecases.NewJobUseCase(h.jobRepository)
-	job, err := jobUseCase.CreateJob(&input)
+	job, err := h.jobUseCase.CreateJob(&input)
 
 	if err != nil {
-		sendError(w, http.StatusInternalServerError, errors.New("error creating job"))
+		sendError(w, http.StatusInternalServerError, err)
 		return
 	}
 
@@ -42,8 +39,7 @@ func (h *JobHandler) Create(w http.ResponseWriter, r *http.Request) {
 func (h *JobHandler) Get(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
 
-	jobUseCase := usecases.NewJobUseCase(h.jobRepository)
-	job, err := jobUseCase.FindJobByID(id)
+	job, err := h.jobUseCase.FindJobByID(id)
 
 	if err != nil {
 		sendError(w, http.StatusNotFound, fmt.Errorf("job with id %s not found", id))
@@ -54,8 +50,7 @@ func (h *JobHandler) Get(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *JobHandler) List(w http.ResponseWriter, r *http.Request) {
-	jobUseCase := usecases.NewJobUseCase(h.jobRepository)
-	jobs, err := jobUseCase.FindAllJobs(0, 0, "", "")
+	jobs, err := h.jobUseCase.FindAllJobs(0, 0, "", "")
 
 	if err != nil {
 		sendError(w, http.StatusInternalServerError, err)
@@ -74,8 +69,7 @@ func (h *JobHandler) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	jobUseCase := usecases.NewJobUseCase(h.jobRepository)
-	job, err := jobUseCase.UpdateJob(id, &input)
+	job, err := h.jobUseCase.UpdateJob(id, &input)
 
 	if err != nil {
 		sendError(w, http.StatusInternalServerError, err)
@@ -88,15 +82,14 @@ func (h *JobHandler) Update(w http.ResponseWriter, r *http.Request) {
 func (h *JobHandler) Delete(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
 
-	jobUseCase := usecases.NewJobUseCase(h.jobRepository)
-	_, err := jobUseCase.FindJobByID(id)
+	_, err := h.jobUseCase.FindJobByID(id)
 
 	if err != nil {
 		sendError(w, http.StatusNotFound, fmt.Errorf("job with id %s not found", id))
 		return
 	}
 
-	if err := jobUseCase.DeleteJob(id); err != nil {
+	if err := h.jobUseCase.DeleteJob(id); err != nil {
 		sendError(w, http.StatusInternalServerError, fmt.Errorf("error deleting job with id %s", id))
 		return
 	}
---
+++ internal/infra/http/handler/user_handler.go
@@ -0,0 +1,80 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/mbrunos/go-hire/internal/core/dto"
+	"github.com/mbrunos/go-hire/internal/core/usecases"
+)
+
+type UserHandler struct {
+	userUseCase *usecases.UserUseCase
+}
+
+func NewUserHandler(usecase *usecases.UserUseCase) *UserHandler {
+	return &UserHandler{
+		userUseCase: usecase,
+	}
+}
+
+func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var input dto.CreateUserInputDTO
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		sendError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	user, err := h.userUseCase.CreateUser(&input)
+
+	if err != nil {
+		sendError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	sendSuccess(w, http.StatusCreated, user)
+}
+
+func (h *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
+	email := r.PathValue("email")
+
+	user, err := h.userUseCase.FindUserByEmail(email)
+
+	if err != nil {
+		sendError(w, http.StatusNotFound, err)
+		return
+	}
+
+	sendSuccess(w, http.StatusOK, user)
+}
+
+func (h *UserHandler) Update(w http.ResponseWriter, r *http.Request) {
+	var input dto.UpdateUserInputDTO
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		sendError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	id := r.PathValue("id")
+	user, err := h.userUseCase.UpdateUser(id, &input)
+
+	if err != nil {
+		sendError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	sendSuccess(w, http.StatusOK, user)
+}
+
+func (h *UserHandler) Delete(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	err := h.userUseCase.DeleteUser(id)
+
+	if err != nil {
+		sendError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	sendSuccess(w, http.StatusNoContent, nil)
+}
